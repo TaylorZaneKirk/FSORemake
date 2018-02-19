d386814744dd1070123a2efda43ff58bdf306d2d
Taylor <TaylorZaneKirk@gmail.com>
2018-02-19 07:49:15 -0700

    test

--- main.js
+++ main.js
@@ -152,8 +152,8 @@ function update() {
     //TODO!!!!!! BELOW LINES DO NOT WORK
     
     console.log(game.global.player);
-    if (game.global.player.lastUpdated + 10 < new Date().getTime() ){
-        console.log(game.global.player.lastUpdated + 10 + " " + new Date().getTime())
+    if (game.global.player.lastUpdated.getTime() + 100000000 < new Date().getTime() ){
+        console.log(game.global.player.lastUpdated.getTime() + 100000000 + " " + new Date().getTime())
         console.log("Requesting new state");
         eurecaProxy.requestUpdate(game.global.myId);
     }
