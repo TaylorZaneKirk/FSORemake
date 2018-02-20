dc78e7b055842b8c794b760b410ec47f4a0217d6
Taylor <TaylorZaneKirk@gmail.com>
2018-02-19 17:25:06 -0700

    same

--- player.js
+++ player.js
@@ -1,13 +1,13 @@
 var PlayerObject = PlayerObject || {};
 
-function PlayerObject(id, game){
-    var game = game; //Reference to game object
-    var id = id; //Local player's id
+function PlayerObject(idRef, gameRef){
+    var game = gameRef; //Reference to game object
+    var id = idRef; //Local player's id
     var playerState = game.global.player; //player's current state
     
     return {
-        game: this.game,
-        id: this.id,
-        playerState: this.playerState
+        game: game,
+        id: id,
+        playerState: playerState
     };
 }
