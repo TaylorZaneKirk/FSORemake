4fc49787bfa4166a188ef0c3e17a1e10c03b66a6
Taylor <TaylorZaneKirk@gmail.com>
2018-02-19 18:09:32 -0700

    Previous test was success; attempting first test to display graphics

--- player.js
+++ player.js
@@ -4,18 +4,21 @@ var PlayerObject = function(idRef, gameRef){
     var game = null; //Reference to game object
     var id = null; //Local player's id
     var playerState = null; //player's current state
+    var playerSprite = null;
 
     function init(idRef, gameRef){
         game = gameRef;
         id = idRef;
         playerState = game.global.player;
+        playerSprite = game.add.sprite(playerState.pos.x, playerState.pos.y, 'player');
+        playerSprite.frame = 0;
     }
 
     function update(){
         if(playerState != game.global.player){
             playerState = game.global.player;
-            console.log("replaced");
-            console.log(playerState);
+
+            //Update global reference
             game.global.localPlayerObject = {
                 game: game,
                 id: id,
