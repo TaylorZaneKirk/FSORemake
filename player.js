a172e1a5398fd5b577369ebaabc8eb367cc53d6c
Taylor <TaylorZaneKirk@gmail.com>
2018-02-21 12:12:22 -0700

    same

--- player.js
+++ player.js
@@ -26,7 +26,7 @@ var PlayerObject = function(idRef, gameRef){
         playerSprite = game.add.sprite(playerState.pos.x, playerState.pos.y, 'player');
         playerSprite.anchor.setTo(0.125,0);
         playerSprite.enableBody = true;
-        playerSprite.body.collideWorldBounds = true;
+        //playerSprite.body.collideWorldBounds = true;
         playerSprite.body.immovable = false;
         playerSprite.body.bounce.setTo(0, 0);
         playerSprite.body.setSize(
