8c07da428b8b8175cbed265123a4761d583c5310
Taylor <TaylorZaneKirk@gmail.com>
2018-02-21 12:18:12 -0700

    same

--- player.js
+++ player.js
@@ -23,11 +23,11 @@ var PlayerObject = function(idRef, gameRef){
         leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
-        playerSprite = gameRef.add.sprite(playerState.pos.x, playerState.pos.y, 'player');
+        playerSprite = game.add.sprite(playerState.pos.x, playerState.pos.y, 'player');
         playerSprite.anchor.setTo(0.125,0);
         playerSprite.enableBody = true;
         //playerSprite.body.collideWorldBounds = true;
-        playerSprite.body.immovable = false;
+        /* playerSprite.body.immovable = false;
         playerSprite.body.bounce.setTo(0, 0);
         playerSprite.body.setSize(
             playerSprite.body.width * 0.6,
@@ -35,7 +35,7 @@ var PlayerObject = function(idRef, gameRef){
             playerSprite.body.width * 0.2,
             playerSprite.body.height * 0.5
         );
-        playerSprite.inputEnabled = true;
+        playerSprite.inputEnabled = true; */
         playerSprite.animations.add('walk-E', [0,1,2], 6, false);
         playerSprite.animations.add('walk-W', [4,5,6], 6, false);
         playerSprite.animations.add('walk-N', [8,9,10], 6, false);
