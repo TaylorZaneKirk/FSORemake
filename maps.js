a1be3fd6626525af8ddefd3a1869bf1cf5eff4f1
Taylor <TaylorZaneKirk@gmail.com>
2018-02-20 19:20:36 -0700

    same

--- maps.js
+++ maps.js
@@ -6,13 +6,9 @@ var changeMap = function(myMap, map, layer) {
     var index = 0;
     for (var x = 0; x < 17; x++){
         for (var y = 0; y < 12; y++) {
-            if(myMap[index] == '\n'){
-                index++;
-                if(y > 0){ --y; }
-            }
-            if(myMap[index] == ';'){
-                index++;
-                --y;
+            if(myMap[index] == ';' || myMap[index] == '\n'){
+                //index++;
+                y--;
                 console.log("here");
             }
             else{
