2ca9b880eabfec0616058830ac955dc1ad07c76a
Taylor <TaylorZaneKirk@gmail.com>
2018-02-21 12:51:13 -0700

    lol

--- player.js
+++ player.js
@@ -23,7 +23,7 @@ var PlayerObject = function(idRef, gameRef){
         leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
-        playerSprite = game.add.sprite(game, playerState.pos.x+1, playerState.pos.y+1, 'player');
+        playerSprite = game.add.sprite(playerState.pos.x+1, playerState.pos.y+1, 'player');
         game.physics.arcade.enable(playerSprite);
         playerSprite.anchor.setTo(0.125,0);
         playerSprite.enableBody = true;
@@ -54,8 +54,6 @@ var PlayerObject = function(idRef, gameRef){
         if(!ready){
             ready = true;
         }
-
-        if(playerSprite == null || playerSprite == undefined){ return; }
 
         if(!ready) { return; }
 
