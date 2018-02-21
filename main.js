12f39509e58513f54dd382de082b7522d2d24d32
Taylor <TaylorZaneKirk@gmail.com>
2018-02-20 21:18:32 -0700

    same

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function init() {
 //THIS NEXT!!!
 function preload() {
     game.load.image('tileset', 'assets/tiles/FSORemakeMapTileset.png');
-    game.load.spritesheet('player', 'assets/PlayerSheet.png', 45, 45);
+    game.load.spritesheet('player', 'assets/PlayerSheet.png', 44, 45);
     //game.load.image('clown', 'assets/images/clown.png');
     //game.load.image('portal', 'assets/images/portal.png');
     //game.global.easystar = new EasyStar.js();   //start the pathfinder
