2b3e6fa508f49517fc154e8634201fa9e56ae85d
Taylor <TaylorZaneKirk@gmail.com>
2018-02-20 17:56:13 -0700

    same

--- maps.js
+++ maps.js
@@ -1,5 +1,5 @@
 //Convert boolean 2D array into tilemap
-var drawMap = function(myMap, map) {
+var changeMap = function(myMap, map) {
 
     //Based on final map configuration, draw the tiles
     var index = 0;
