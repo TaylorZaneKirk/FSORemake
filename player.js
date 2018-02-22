0551ad1a6af88fa22f540e5a4cffc90a9f2a4f07
Taylor <TaylorZaneKirk@gmail.com>
2018-02-21 19:40:33 -0700

    same

--- player.js
+++ player.js
@@ -131,7 +131,7 @@ var PlayerObject = function(idRef, gameRef){
         //playerTween.stop();
         playerSprite.x = (playerState.pos.x+1)*32;
         playerSprite.y = (playerState.pos.y+1)*32;
-        playerTween.to({x: (playerState.pos.x+1) * 32, y: (playerState.pos.y+1) * 32}, 250, null, true);
+        //playerTween.to({x: (playerState.pos.x+1) * 32, y: (playerState.pos.y+1) * 32}, 250, null, true);
         //playerTween.resume();
         //playerSprite.play(playerState.playerAction + '-' + playerState.playerFacing);
         //gameRef.add.tween(playerSprite).resume();
