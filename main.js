934bd948322c9e9b358079b30c9ea724d3de21a3
Taylor <TaylorZaneKirk@gmail.com>
2018-02-22 16:08:36 -0700

    same

--- main.js
+++ main.js
@@ -142,6 +142,7 @@ function create() {
 
 function update() {
     if (!game.global.ready || !game.global.player || game.global.localPlayerObject == {} || client.eurecaProxy == undefined){
+        console.log("gone wrong");
         return; //Stuff isn't ready; hold on...
     }
 
