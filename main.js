de14ed0aa842ec12e1bbcd3d8c662d0e12ebb7a3
Taylor <TaylorZaneKirk@gmail.com>
2018-02-23 13:04:14 -0700

    same

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ function initMultiPlayer(game, globals){
             
             globals.player = state;
             globals.playerList[state.playerName] = {player: state};
-            globals.localPlayerObject = new PlayerObject(state.playerName, game);
+            globals.localPlayerObject, globals.playerList[state.playerName] = new PlayerObject(state.playerName, game);
             game.global.ready = true;
             changeMap(state.mapData, map, layerFirst);
         }
