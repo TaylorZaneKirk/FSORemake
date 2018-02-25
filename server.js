462f2327ee874fa8cac3cf615b3deb861e02e911
Taylor <TaylorZaneKirk@gmail.com>
2018-02-25 10:57:46 -0700

    same

--- server.js
+++ server.js
@@ -279,3 +279,17 @@ loadMapData = function(){
     });
 }
 
+Object.filter = function( obj, predicate) {
+    var result = {}, key;
+    // ---------------^---- as noted by @CMS, 
+    //      always declare variables with the "var" keyword
+
+    for (key in obj) {
+        if (obj.hasOwnProperty(key) && !predicate(obj[key])) {
+            result[key] = obj[key];
+        }
+    }
+
+    return result;
+};
+
