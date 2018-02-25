aa30bb68979d0ab2825b9e8a93a5936e64a54111
Taylor <TaylorZaneKirk@gmail.com>
2018-02-25 11:50:13 -0700

    same

--- server.js
+++ server.js
@@ -197,13 +197,14 @@ eurecaServer.updateClients = function (id) {
     
             //players[i].state.playersVisible = worldMap[players[i].state.worldX + '-' + players[i].state.worldY].players;
             players[i].state.playersVisible = Object.filter(worldMap[players[i].state.worldX + '-' + players[i].state.worldY].players, player => player.playerName != players[i].id);
+            console.log(players[i].state.playersVisible)
             remote.recieveStateFromServer(players[i].state);
         }
        
     }
 
     players[id].state.playersVisible = Object.filter(worldMap[players[id].state.worldX + '-' + players[id].state.worldY].players, player => player.playerName != id);
-
+    console.log(players[id].state.playersVisible);
     newRemote.recieveStateFromServer(players[id].state);
 
     /* for(var i in allPlayerStates){
