ee385880e894ce0a01e4f9dc54294c3beb3bda53
Taylor <TaylorZaneKirk@gmail.com>
2018-02-26 17:25:23 -0700

    same

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ var menuState = {
     create: function(){
 
         //var guiMenu = game.add.sprite(game.world.centerX, game.world.centerY, 'menuScreen');
-        var guiLoadCharacter = game.add.sprite(game.world.centerX, game.world.centerY + (game.world.centerY * 0.1))
+        var guiLoadCharacter = game.add.sprite(game.world.centerX, game.world.centerY);
         //guiMenu.anchor.set(0.5);
         guiLoadCharacter.anchor.set(0.5);
         guiLoadCharacter.inputEnabled = true;
