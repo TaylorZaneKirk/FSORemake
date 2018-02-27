a3582d5a98cf1f4979406dd1f82dd67fb13d2d72
Taylor <TaylorZaneKirk@gmail.com>
2018-02-27 13:04:17 -0700

    same

--- loadPlayer.js
+++ loadPlayer.js
@@ -2,7 +2,7 @@ var loadPlayerState = {
     
     create: function(){
         var loginScreen = game.add.sprite(game.world.centerX, game.world.centerY, 'loginScreen');
-        var cancelButton = game.add.sprite(game.world.centerX * 1.02225, game.world.centerY * 1.125, 'cancelButton');
+        var cancelButton = game.add.sprite(game.world.centerX * 1.12225, game.world.centerY * 1.125, 'cancelButton');
         var confirmButton = game.add.sprite(game.world.centerX * 1.22225, game.world.centerY * 1.125, 'confirmButton');
         loginScreen.anchor.set(0.5);
         cancelButton.anchor.set(0.5);
