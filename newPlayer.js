d686ae1a7eb1a3ba77d2bafea454a700891ee804
Taylor <TaylorZaneKirk@gmail.com>
2018-02-28 20:12:43 -0700

    same

--- newPlayer.js
+++ newPlayer.js
@@ -1,7 +1,7 @@
 var newPlayerState = {
     create: function(){
         var createScreen = game.add.sprite(game.world.centerX, game.world.centerY, 'createScreen');
-        var cancelButton = game.add.sprite(game.world.centerX * 1.5, game.world.centerY * 1.29, 'cancelButton2');
+        var cancelButton = game.add.sprite(game.world.centerX * 1.55, game.world.centerY * 1.29, 'cancelButton2');
         var confirmButton = game.add.sprite(game.world.centerX * 1.6975, game.world.centerY * 1.29, 'confirmButton2');
         inputUsername = game.add.inputField(game.world.centerX * 1.435, game.world.centerY * 0.799, {
             backgroundColor: '#c0c0c0',
