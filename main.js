a0f8d40f8e4c3756a8f5520f37bb847b75c65f8c
Taylor <TaylorZaneKirk@gmail.com>
2018-03-02 16:54:48 -0700

    same

--- main.js
+++ main.js
@@ -32,10 +32,9 @@ var mainState = {
             wordWrap: true,
             wordWrapWidth: 762,
             width: '762px',
-            height: '200px',
+            row: 4,
         }
         var chatBox = game.add.text(game.world.width * 0.022, game.world.centerY, chatLog, style);
-        chatBox.height = 200;
         //chatBox.setTextBounds(16, 16, 300, 762);
         
         chatInput = game.add.inputField(game.world.width * 0.022, game.world.bottom * 0.9251, {
