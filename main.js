79c6eb104677c1e7e87b6e545247731cc7144848
Taylor <TaylorZaneKirk@gmail.com>
2018-03-02 11:59:17 -0700

    same

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ var mainState = {
         game.global.mapManager = new MapManager(game);
         var leftPanel = game.add.sprite(0, game.world.bottom * 0.099, 'leftPanel');
         var topPanel = game.add.sprite(0, 0, 'topPanel');
-        var rightMiddlePanel = game.add.sprite(game.world.width * 0.88, game.world.centerY * 0.6, 'rightMiddlePanel');
+        var rightMiddlePanel = game.add.sprite(game.world.width * 0.88, game.world.centerY * 0.65, 'rightMiddlePanel');
         rightMiddlePanel.anchor.set(0.5);
         var rightPanel = game.add.sprite(game.world.width * 0.7063, 0, 'rightPanel');
         var bottomPanel = game.add.sprite(game.world.centerX * 0.7475, game.world.bottom * 0.8365, 'bottomPanel');
