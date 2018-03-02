4d6458c0c82b20aa1256bb923122fbfb84881dbe
Taylor <TaylorZaneKirk@gmail.com>
2018-03-01 20:56:34 -0700

    same

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ var mainState = {
         //Maps and layers
         game.global.mapManager = new MapManager(game);
         var topPanel = game.add.sprite(0, 0, 'topPanel');
-        var rightPanel = game.add.sprite(game.world.right * 0.98215, 0, 'rightPanel');
+        var rightPanel = game.add.sprite(game.world.right * 0.9821, 0, 'rightPanel');
     },
 
     update: function(){
