7bb97bab7edce6c98aaa42e6751a7bc0c6b0251f
Taylor <TaylorZaneKirk@gmail.com>
2018-03-01 21:54:02 -0700

    same

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var mainState = {
         var leftPanel = game.add.sprite(0, game.world.bottom * 0.099, 'leftPanel');
         var topPanel = game.add.sprite(0, 0, 'topPanel');
         var rightPanel = game.add.sprite(game.world.right * 0.981, 0, 'rightPanel');
-        var bottomPanel = game.add.sprite(game.world.centerX * 0.7475, game.world.bottom * 0.836, 'bottomPanel');
+        var bottomPanel = game.add.sprite(game.world.centerX * 0.7475, game.world.bottom * 0.837, 'bottomPanel');
         bottomPanel.anchor.set(0.5);
         
     },
