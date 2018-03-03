1472141acbdeb8ce8e788f2372395df4412d8e5b
Taylor <TaylorZaneKirk@gmail.com>
2018-03-02 19:56:54 -0700

    same

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ var mainState = {
         var bottomPanel = game.add.sprite(game.world.centerX * 0.7475, game.world.bottom * 0.8365, 'bottomPanel');
         bottomPanel.anchor.set(0.5);
 
-        chatBox = game.add.inputField(game.world.width * 0.022, game.world.bottom * 0.8, {
+        chatBox = game.add.inputField(game.world.width * 0.022, game.world.bottom * 0.75, {
             backgroundColor: '#494745',
             width: 762,
             height: 75,
