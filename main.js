52e68e8d4c3ba7d83fbd4230cf61894923e44ff2
Taylor <TaylorZaneKirk@gmail.com>
2018-03-03 15:22:37 -0700

    maybe?

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ function initMultiPlayer(game, globals){
         //chatLog += '\n' + message;
         //chatBox.setText(chatLog);
         var textStyle = {font:"14px Arial", fill:"white"};
-        var text = game.make.text(game.world.width * 0.022, game.world.bottom * 0.76, message, textStyle);
+        var text = game.make.text(0, scroller.length + 1, message, textStyle);
         scroller.addChild(text);
     }
 
