c3ef4213dbcb79ee98ae1b3f3ae81a682b91c873
Taylor <TaylorZaneKirk@gmail.com>
2018-03-03 17:01:13 -0700

    same

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ var mainState = {
         cropRect.drawRect(game.world.width * 0.022, game.world.bottom * 0.76, 762, 88);
         cropRect.endFill();
 
+        container = game.add.sprite(game.world.width * 0.022, game.world.bottom * 0.76);
         container.mask = cropRect;
 
         scroller = game.add.existing(new ScrollableArea(game.world.width * 0.022, game.world.bottom * 0.76, 762, 88));
