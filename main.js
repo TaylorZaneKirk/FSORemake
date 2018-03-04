444627b0765db977354c929e7c7e375d532047cc
Taylor <TaylorZaneKirk@gmail.com>
2018-03-03 21:59:56 -0700

    same

--- main.js
+++ main.js
@@ -137,6 +137,7 @@ function initMultiPlayer(game, globals){
         var text = game.make.text(0, scroller.length * 22, message, textStyle);
         console.log(text);
         scroller.addChild(text);
+        scroller.scrollTo(0, scroller.length * 22);
         //chatLog += '/n' + message;
     }
 
