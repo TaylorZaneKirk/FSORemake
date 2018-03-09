78d644ab7fe23fdbdb7a0330c507485e995a7c68
Taylor <TaylorZaneKirk@gmail.com>
2018-03-08 18:03:47 -0700

    same

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ var mainState = {
         bottomPanel.anchor.set(0.5);
 
         pageButtons = game.add.group();
-        var statsButtonInactive = game.add.sprite(game.world.width * 0.77, game.world.centerY * 0.0725, 'statsButtonInactive');
+        var statsButtonInactive = game.add.sprite(game.world.width * 0.77, game.world.centerY * 0.0720, 'statsButtonInactive');
         pageButtons.add(statsButtonInactive);
 
         //Stats Page
