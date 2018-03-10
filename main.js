ac43785d7b1986ab599da6ddbbd5d37396da0488
Taylor <TaylorZaneKirk@gmail.com>
2018-03-10 08:34:33 -0700

    same

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ var mainState = {
         //Skills Page
         skillsPage = game.add.existing(new ScrollableArea(game.world.width * 0.765, game.world.centerY * 0.4, 117, 300, { horizontalScroll: false, verticalScroll: true, horizontalWheel: false, verticalWheel: true, kineticMovement: false }));
         skillsPage.inputEnableChildren = true;
-        var textStyle = {font:"bold 14px Arial", fill:"white", cursor:"grab"};
+        var textStyle = {font:"bold 14px Arial", fill:"white"};
         skillsPage.addChild(game.make.text(0, skillsPage.length * 22, "Swordsmanship", textStyle));
         skillsPage.addChild(game.make.text(0, skillsPage.length * 22, "Mysticism", textStyle));
         skillsPage.addChild(game.make.text(0, skillsPage.length * 22, "Archery", textStyle));
