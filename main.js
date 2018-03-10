cffabb2bfbcdf350ae1d9176f8fc8267c15bc86c
Taylor <TaylorZaneKirk@gmail.com>
2018-03-10 08:44:01 -0700

    same

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ var mainState = {
         statsPage.addChild(luckLabel);
 
         //Skills Page
-        skillsPage = game.add.existing(new ScrollableArea(game.world.width * 0.765, game.world.centerY * 0.4, 100, 300, { horizontalScroll: false, verticalScroll: true, horizontalWheel: false, verticalWheel: true, kineticMovement: false }));
+        skillsPage = game.add.existing(new ScrollableArea(game.world.width * 0.765, game.world.centerY * 0.4, 125, 300, { horizontalScroll: false, verticalScroll: true, horizontalWheel: false, verticalWheel: true, kineticMovement: false }));
         //skillsPage.inputEnableChildren = true;
         skillsPage.maskGraphics.input.useHandCursor = true;
         var textStyle = {font:"bold 14px Arial", fill:"white"};
