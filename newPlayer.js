ce3c47d8985bbb900f97954da5b1db1e295bc94e
Taylor <TaylorZaneKirk@gmail.com>
2018-03-11 10:02:55 -0600

    same

--- newPlayer.js
+++ newPlayer.js
@@ -248,7 +248,7 @@ var newPlayerState = {
         playerModel = game.add.sprite(game.world.centerX * 1.07, game.world.centerY, 'player');
         playerModel.frame = 13;
 
-        availablePointsText = game.add.text( game.world.centerX * 0.32, game.world.centerY * 1.172, "Available Points Remaining: " + availablePoints, { font: "bold 10px Arial", fill: "#FFF",  boundsAlignH: "center", boundsAlignV: "middle" } );
+        availablePointsText = game.add.text( game.world.centerX * 0.32, game.world.centerY * 1.185, "Available Points Remaining: " + availablePoints, { font: "bold 10px Arial", fill: "#FFF",  boundsAlignH: "center", boundsAlignV: "middle" } );
         //createScreen.addChild(availablePointsText);
         console.log(availablePointsText);
     }
