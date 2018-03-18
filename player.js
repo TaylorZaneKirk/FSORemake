dc53725c174299178e34d9bcd8bad53a99df5d94
Taylor <TaylorZaneKirk@gmail.com>
2018-03-18 11:18:14 -0600

    same

--- player.js
+++ player.js
@@ -30,7 +30,7 @@ var PlayerObject = function(idRef, gameRef){
         if(playerState.gender == 'm'){
             //playerSprite = game.add.sprite((playerState.pos.x+1)*32, (playerState.pos.y+1)*32, 'player');
             playerSprite = game.add.sprite((playerState.pos.x+1)*32, (playerState.pos.y+1)*32, 'defaultBody');
-            playerHead = game.add.sprite(-1, -1, 'maleHead1');
+            playerHead = game.add.sprite(-32, -32, 'maleHead1');
             playerSprite.addChild(playerHead);
         }
         else{
