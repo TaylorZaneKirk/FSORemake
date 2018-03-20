17c53711554913d4a1d733e7f9807593130a2bef
Taylor <TaylorZaneKirk@gmail.com>
2018-03-20 09:00:33 -0600

    testing

--- maps.js
+++ maps.js
@@ -45,7 +45,8 @@ var MapManager = class MapManager {
 
         for(var i in this.mapData.items){
             console.log(this.mapData.items[i].itemName);
-            var item = game.add.sprite((this.mapData.items[i].pos.x+1) * 32, (this.mapData.items[i].pos.y+1) * 32, this.mapData.items[i].itemName);
+            var thisTile = this.map.getTile(this.mapData.items[i].pos.x+1, this.mapData.items[i].pos.y+1, this.layers[0], true);
+            var item = game.add.sprite(thisTile.worldX, thisTile.worldY, this.mapData.items[i].itemName); //adjust position
             item.anchor.setTo(0.5);
         }
     }
