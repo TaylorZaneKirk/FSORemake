a0e427e3b82b662daa9876c780e1889e813cdf6a
Taylor <TaylorZaneKirk@gmail.com>
2018-03-25 10:45:08 -0600

    same

--- player.js
+++ player.js
@@ -134,7 +134,7 @@ var PlayerObject = function(idRef, gameRef){
         playerLeft.frame = playerSprite.frame;
 
         playerTween = game.add.tween(playerSprite);
-        playerImage = playerSprite;
+        playerImage = Object.assign({}, playerSprite);
         console.log(playerImage);
         playerImage.position = {x: game.world.width * 0.875, y: game.world.centerY * 1.14};
         playerImage.play('idle-S');
