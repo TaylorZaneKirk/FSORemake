f6196c746eec1b3593f9fc5ea4f1fe194de8dfef
Taylor <TaylorZaneKirk@gmail.com>
2018-03-25 10:56:56 -0600

    same

--- player.js
+++ player.js
@@ -134,10 +134,10 @@ var PlayerObject = function(idRef, gameRef){
         playerLeft.frame = playerSprite.frame;
 
         playerTween = game.add.tween(playerSprite);
-        playerImage = Object.assign(new Sprite(), playerSprite);
+        playerImage = game.add.sprite(game.world.width * 0.875, game.world.centerY * 1.14, playerSprite.generateTexture);
         console.log(playerImage);
-        playerImage.position = {x: game.world.width * 0.875, y: game.world.centerY * 1.14};
-        playerImage.play('idle-S');
+        //playerImage.position = {x: game.world.width * 0.875, y: game.world.centerY * 1.14};
+        //playerImage.play('idle-S');
     }
 
     function showPlayerName(state){
