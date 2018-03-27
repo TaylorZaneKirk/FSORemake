d3597c3571f0136159e7c865c948d2c5c2888f8c
Taylor <TaylorZaneKirk@gmail.com>
2018-03-27 17:46:34 -0600

    same

--- server.js
+++ server.js
@@ -528,14 +528,13 @@ class PlayerState
             this.inventory[slotNumber - 1].amount -= 1;
             worldMap[this.worldX + '-' + this.worldY].items[itemUnderneath.locationId].amount += 1;
             console.log("decrease and increase");
-            console.log(worldMap[this.worldX + '-' + this.worldY].items[itemUnderneath.locationId]);
-            console.log(itemUnderneath.locationId)
             con.query("UPDATE worldItems SET amount=" +
                 + worldMap[this.worldX + '-' + this.worldY].items[itemUnderneath.locationId].amount + 
                 " WHERE locationId='" + itemUnderneath.locationId + "'", function (err, result, fields) {
                     if (err) throw err;
                     //remove from inventory
-                    //console.log(this.inventory[slotNumber - 1]);
+                    console.log(this.inventory[slotNumber - 1]);
+                    console.log(slotNumber);
                     //con.query("UPDATE playerInv SET slot" + slotNumber + "Amount=" + this.inventory[slotNumber - 1].amount + " WHERE username = '" + this.username + "'", function (err, result, fields){if (err) throw err;});
             });
         }
