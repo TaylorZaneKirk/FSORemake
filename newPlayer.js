cc37cedfaff85e147e3da4087c387ced55a0d0be
Taylor <TaylorZaneKirk@gmail.com>
2018-03-28 17:06:12 -0600

    same

--- newPlayer.js
+++ newPlayer.js
@@ -291,7 +291,7 @@ var newPlayerState = {
         playerModel.frame = 13;
         playerHead.frame = 13;
 
-        leftArrow = game.add.sprite(game.world.centerX * 1.05, game.world.centerY * 1.145, 'leftArrow');
+        leftArrow = game.add.sprite(game.world.centerX * 1.05, game.world.centerY * 1.1475, 'leftArrow');
     }
 }
 
