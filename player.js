fbb9b1e427284155c222483b136d8add68f9c6ff
Taylor <TaylorZaneKirk@gmail.com>
2018-03-28 21:57:02 -0600

    playerLayer implementation

--- player.js
+++ player.js
@@ -181,6 +181,8 @@ var PlayerObject = function(idRef, gameRef){
         playerLeft.frame = playerSprite.frame;
 
         playerTween = game.add.tween(playerSprite);
+        game.global.playerLayer.add(playerSprite);
+
         playerImage.head.frame = 13;
         playerImage.torso.frame = 13;
         playerImage.right.frame = 13;
