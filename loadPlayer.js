07077459cfac32bcc2ef8d508d8d6ddf05a71f2b
Taylor <TaylorZaneKirk@gmail.com>
2018-03-28 21:49:56 -0600

    same

--- loadPlayer.js
+++ loadPlayer.js
@@ -70,8 +70,8 @@ function queryLogin() {
         // Assign my new connection Id
         game.global.myId = id;
         game.global.itemManager = new itemManager(itemData);
-        game.global.itemLayer = game.group.add();
-        game.global.playerLayer = game.group.add();
+        game.global.itemLayer = game.add.group();
+        game.global.playerLayer = game.add.group();
         //tell server client is ready
         //globals.eurecaProxy.initPlayer(id);
         game.state.start('main');
