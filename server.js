41cce77363759ce1c0d839f6d877ab13b4b7f86c
Taylor <TaylorZaneKirk@gmail.com>
2018-03-31 14:03:12 -0600

    same

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var Eureca = require('eureca.io');
 
 
 //create an instance of EurecaServer
-var eurecaServer = new Eureca.Server({allow:['setId', 'recieveStateFromServer', 'kill', 'disconnect', 'errorAndDisconnect', 'recieveBroadcast', 'removeItem', 'placeItem']});
+var eurecaServer = new Eureca.Server({allow:['setId', 'recieveStateFromServer', 'kill', 'disconnect', 'errorAndDisconnect', 'recieveBroadcast', 'removeItem', 'placeItem', 'placeNPC']});
 
 //attach eureca.io to our http server
 eurecaServer.attach(server);
