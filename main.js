3e98385801433cc19e6ac9fa8966f6388b602a43
Taylor <TaylorZaneKirk@gmail.com>
2018-04-07 15:57:41 -0600

    same

--- main.js
+++ main.js
@@ -434,7 +434,6 @@ function initMultiPlayer(game, globals){
     }
 
     client.exports.placeNPC = function(npc){
-        console.log("SPAWNING NPC");
         globals.mapManager.spawnNPC(npc)
     }
 
@@ -461,8 +460,7 @@ function initMultiPlayer(game, globals){
 
             for(var i in globals.npcList){
                 globals.npcList[i].npcObject.npcSprite.kill();
-                delete globals.npcList[i]
-                console.log("DESTORYING NPC");
+                delete globals.npcList[i];
             }
 
             for(var i in globals.items){
