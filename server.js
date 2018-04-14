c6f89d5c6130e259e5e9f18c881707e50e7c59d1
Taylor <TaylorZaneKirk@gmail.com>
2018-04-14 15:12:56 -0600

    same

--- server.js
+++ server.js
@@ -893,9 +893,9 @@ class NPC{
                     willFollow = true;
                     if(this.spells != {} && Math.floor(Math.random() * Math.floor(3)) == 0){
                         console.log('gonna cast a spell!');
-                        var whichSpellIndex = Object.keys[Math.floor(Math.random() * Object.keys(this.spells).length)];
+                        var whichSpellIndex = Math.floor(Math.random() * Object.keys(this.spells).length);
                         console.log(whichSpellIndex);
-                        var whichSpell = this.spells[whichSpellIndex];
+                        var whichSpell = this.spells[Object.keys(this.spells)[whichSpellIndex]];
                         console.log(whichSpell);
                     }
                     
